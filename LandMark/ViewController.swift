83642a9da03bacf50a81600e18a8a590f4e22bf8
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-10-30 19:05:57 -0300

    project init

---
+++ LandMark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandMark
+//
+//  Created by Lucas Migge on 30/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
